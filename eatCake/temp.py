35bfb0e147054dfd1a7e4fa72bf730c7de2d3580
Nishant Patel <nishantpatel@Nishants-MBP.home>
2018-01-09 12:41:04 -0500

    temp has stdIn example

--- eatCake/temp.py
+++ eatCake/temp.py
@@ -5,9 +5,27 @@
 
 
 
-myList = [ 
-		   ["hi", "djh3"],
-		   ["1"], 
-		   ["jdw", "exw", "dw4d", "cwrwr", "rxwcrw"] 
-		 ]
-print len(min(myList))
+# myList = [ 
+# 		   ["hi", "djh3"],
+# 		   ["1"], 
+# 		   ["jdw", "exw", "dw4d", "cwrwr", "rxwcrw"] 
+# 		 ]
+# print len(min(myList))
+
+
+import sys
+
+def printArgs(args):
+    print "THe user input the following: "
+    for arg in args:
+        print arg
+        
+inputList = []
+while True:
+	userInput = raw_input()
+	if userInput == "":
+		break
+	else:
+		inputList.append(userInput)
+
+printArgs(inputList)
