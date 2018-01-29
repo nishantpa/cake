068be133a5a3d21ce90106953254f5245dae390d
Nishant Patel <nishantpatel@Nishants-MacBook-Pro.local>
2018-01-29 13:16:06 -0500

    simple brackets validator

--- leet/balance.py
+++ leet/balance.py
@@ -9,15 +9,41 @@
 # "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
 
 def validator(case):
-	for char in case:
-		print "JHelo"	
+	validatorStack = []
+	
+	if not validatorStack:
+		validatorStack.append(case[0])
+	try:
+		for char in case[1:]:
+			if char == ')':
+				leftParen = validatorStack.pop()
+				if leftParen != '(':
+					print "Unbalanced!: ", leftParen
+					return
+			
+			elif char == '}':
+				leftBrace = validatorStack.pop()
+				if leftBrace != '{':
+					print "Unbalanced!: ", leftBrace
+					return
+
+			elif char == ']':
+				leftBracket = validatorStack.pop()
+				if leftBracket != '{':
+					print "Unbalanced!: ", leftBracket
+					return
+
+			else:
+				validatorStack.append(char)
+
+	except IndexError:
+		print "Unbalanced! IndexError is evidence."
 
 
-
-
-case1 = "()"
+case0 = "()"
+case1 = "())"
 case2 = "()[]{}"
 case3 = "(]"
 case4 = "([)]"
-validator(case1)
 
+validator(case1)
