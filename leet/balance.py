4caec871346b877d008ac5d425efcd15cc776e6f
Nishant Patel <nishantpatel@Nishants-MacBook-Pro.local>
2018-01-29 12:09:39 -0500

    start challenge

---
+++ leet/balance.py
@@ -0,0 +1,23 @@
+# Swami Shreeji
+# Bracket validator - leetcode
+
+# Given a string containing just the characters 
+# '(', ')', '{', '}', '[' and ']', 
+# determine if the input string is valid.
+
+# The brackets must close in the correct order, 
+# "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
+
+def validator(case):
+	for char in case:
+		print "JHelo"	
+
+
+
+
+case1 = "()"
+case2 = "()[]{}"
+case3 = "(]"
+case4 = "([)]"
+validator(case1)
+
