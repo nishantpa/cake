1d5c01b15a27b29b656b640473a0b25f179d2c42
Nishant Patel <nishantpatel@Nishants-MacBook-Pro.local>
2018-01-29 11:43:45 -0500

    fin but return later

--- eatCake/rotationPoint.py
+++ eatCake/rotationPoint.py
@@ -18,22 +18,58 @@
 # Write a function for finding the index of the "rotation point," which is 
 # where I started working from the beginning of the dictionary.
 
-def rotationPoint (wordsList):
-	print wordsList
+
+# ATTACK PLAN: Binary search on words. It'll be an altered binary search  
+
+def find_rotation_point(wordsList):
+    first_word = wordsList[0]
+    floor_index = 0
+    ceiling_index = len(wordsList) - 1
+
+    while floor_index < ceiling_index:
+        guess_index = floor_index + ((ceiling_index - floor_index) / 2)
+
+        # If guess comes after first word or is the first word
+        if wordsList[guess_index] >= first_word:
+            # Go right
+            print
+            print "Going right"
+            print "floor_index: ", floor_index, wordsList[floor_index]
+            floor_index = guess_index
+        else:
+            # Go left
+            print
+            print "Go left"
+            print "ceiling_index: ", ceiling_index, wordsList[ceiling_index]
+            ceiling_index = guess_index
+
+        # If floor and ceiling have converged
+        if floor_index + 1 == ceiling_index:
+            # Between floor and ceiling is where we flipped to the beginning
+            # so ceiling is alphabetically first
+            print 
+            print "floor and ceiling converge"
+            print "floor_index: ", floor_index, "ceiling_index: ", ceiling_index, wordsList[ceiling_index] 
+            return ceiling_index
+	
 
 
-words = [
-  'ptolemaic',
-  'retrograde',
-  'supplant',
-  'undulate',
-  'xenoepist',
-  'asymptote',  # <-- rotates here!
-  'babka',
-  'banoffee',
-  'engender',
-  'karpatka',
-  'othellolagkage',
+# 13 words
+wordsList = [
+   'ptolemaic'
+  ,'patel'
+  ,'retrograde'
+  ,'supplant'
+  ,'the'
+  ,'undulate'
+  ,'xenoepist'
+  ,'asymptote' # <-- rotates here!
+  ,'babka'
+  ,'banoffee'
+  ,'engender'
+  ,'karpatka'
+  ,'othellolagkage'
+  ,
 ]
 
-rotationPoint(words)
+find_rotation_point(wordsList)
