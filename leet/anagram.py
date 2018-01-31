c9ffb4c422f364a10ae938c60a835c0482cbf1be
Nishant Patel <nishantpatel@Nishants-MacBook-Pro.local>
2018-01-31 09:16:31 -0500

    start new challenges

---
+++ leet/anagram.py
@@ -0,0 +1,3 @@
+# Swami Shreeji
+# Do tomorrow morning
+# https://leetcode.com/problems/group-anagrams/description/
---
+++ leet/twoSum.py
@@ -0,0 +1,9 @@
+# Swami Shreeji
+# Do NOW https://leetcode.com/problems/two-sum/description/
+
+def twoSum(myList, target):
+	print "Hello world"
+
+
+cases = []; target = 9
+twoSum(cases, target)
