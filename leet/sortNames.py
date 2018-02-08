3aa654c4773c74e0767318545746e5ce75417650
Nishant Patel <nishantpatel@Nishants-MBP.home>
2018-02-07 21:01:01 -0500

    fin 2 challenges

---
+++ leet/sortNames.py
@@ -0,0 +1,21 @@
+# Swami Shreeji
+# 7 Feb 2018
+
+# Sorting names, instead of numbers. And get the most frequent name at the start
+
+def sortNames(names):
+	# Check if in dict, increment if yes. Else, add to dict
+	myDict = {}
+	for elem in names:
+		if elem in myDict:
+			myDict[elem] += 1
+		else:
+			myDict[elem] = 1
+
+	# Reverse sort and return based on values
+	return sorted(myDict.items(), key = lambda x: x[1], reverse = True)
+
+
+names = ["Nishant", "Anand", "Anand", "Atmiya", "Atmiya", "Atmiya", "Lallu", "Sahaj", "Sahaj", "Sahil", "Hariprasad"]
+soln = sortNames(names)
+print soln
---
+++ leet/subarrayProd.py
@@ -0,0 +1,34 @@
+# Swami Shreeji
+# 7 Feb 2018
+# Speedrun - 10 min code challenge
+
+# You're passed an array of integers. Your task is to find all the subarrays
+# you can make with that integer, and find the product. After finding the product
+# check to see which products are less than some value k. Return the count of 
+# how many of those subarray products are less than k.
+
+import itertools
+from operator import mul 
+
+temp = []
+def count(numbers, k):
+    i = 0
+    subarrays = []
+    while i < len(numbers):
+        j = 3
+        while j > 0:
+            temp = numbers[i:j]
+            subarrays.append(temp)
+            j -= 1
+        i += 1
+    
+    parseThis = []
+    for elem in subarrays:
+        if len(elem) > 0:
+            parseThis.append(elem)
+    
+    almostThere = []
+    for elem in parseThis:
+        if reduce (mul, elem, 1) < k:
+            almostThere.append(elem)
+    return len(almostThere)
